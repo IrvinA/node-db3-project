5aa259aded094646cef4ae9af2624393e8a4eb55
IrvinA <arevalosirvin@gmail.com>
2021-12-08 10:33:22 -0800

    second middleware complete

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -18,15 +18,21 @@ const checkSchemeId = async (req, res, next) => {
   }
 };
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
+const validateScheme = (req, res, next) => {
+  const { scheme_name } = req.body;
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+  ) {
+    next({
+      status: 400,
+      message: 'invalid scheme_name',
+    });
+  } else {
+    next();
   }
-*/
-const validateScheme = (req, res, next) => {};
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
